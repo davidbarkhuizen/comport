5e4fd95f9418dba64ce700735da4b0355be8747d
david barkhuizen <david@yoco.co.za>
2017-11-05 16:07:29 +0200

    intermediate - in progress with search

---
+++ reactapp/src/index.js
@@ -0,0 +1,188 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import logo_img from './logo.80.png'
+
+import things from './model.json'
+
+// ========================================
+
+function buildModel(things) {
+
+	var tags = things
+		.map((thing) => thing.tags)
+		.reduce((a,b) => a.concat(b), [])
+		.filter((x, i, a) => a.indexOf(x) === i)
+
+	return {
+		tags: tags
+	}
+}
+
+// ========================================
+
+class SearchTag extends React.Component {
+
+	render() {
+		return (
+			<button 
+				onClick={this.props.onClick}
+				className="font-normal tag-button"
+			>{this.props.tag}</button>
+		)
+	}
+}
+
+// ========================================
+
+
+class SearchTagCloud extends React.Component {
+
+	renderTag(tag, handleClick) {
+		return (
+			<SearchTag
+				key={tag}
+				tag={tag}
+				onClick={() => this.props.handleTagClicked(tag)}
+			/>
+		);
+	}
+
+	render() {
+		return this.props.tags.map((tag) => this.renderTag(tag))
+	}
+}
+
+// ========================================
+
+class SearchBox extends React.Component {
+
+	render() {
+		return (
+			<input
+				className="font-normal"
+				value={this.props.searchText} 
+				onChange={evt => this.props.onSearchTextChanged(evt)}
+				placeholder="search..."
+			/>
+		)
+	}
+}
+
+// ========================================
+
+class SummaryItem extends React.Component {
+
+	render() {
+		return (
+			<div>
+				<div>{this.props.thing.name}</div>
+				<div>{this.props.thing.description}</div>
+			</div>
+			)
+	}
+
+}
+
+// ========================================
+
+class SearchResults extends React.Component {
+
+	renderSearchResult(result, handleResultClicked) {
+		return (
+			<SummaryItem
+				key={result.id}
+				thing={result}
+				onClick={() => this.props.handleResultClicked(result)}
+			/>
+		);
+	}
+
+	render() {
+		return this.props.results.map((result) => this.renderSearchResult(result, this.props.handleResultClicked))
+	}
+}
+
+// =======================================
+
+class Search extends React.Component {
+
+	constructor(props) {
+
+		super(props)
+
+		this.state = {
+			searchText: '',
+			searchResults: []
+		}
+	}
+
+	search = (things, token) => {
+
+		return things.filter((thing,i,a) => {
+
+			var matches = false
+
+			if (thing.name.indexOf(token) !== -1)
+				matches = true
+
+			return matches
+		})
+	}
+
+	handleSearchTextChanged = (evt) => {
+
+		const searchText = evt.target.value
+		const searchResults = this.search(this.props.data, searchText)
+
+		const newState = Object.assign({}, this.state, {searchText:searchText}, searchResults:searchResults)
+		this.setState(newState)
+	}
+
+	handleTagClicked = (tag) => {
+		console.log(tag)
+	}
+
+	render() {
+		return (
+			<div>	
+				<div>
+					<SearchTagCloud
+						tags={this.props.tags}
+						handleTagClicked={this.handleTagClicked}
+						className="two-thirds-width"
+					/>
+				</div>	
+				<div className="margin">	
+					<SearchBox
+						searchText={this.state.searchText}
+						onSearchTextChanged={this.handleSearchTextChanged}
+						className="full-width"
+					/>
+				</div>
+				<div>
+					<div>search results</div>
+					<SearchResults
+						results={this.state.searchResults}
+					/>
+				</div>
+			</div>
+		)
+	}
+}
+
+// ========================================
+
+const {tags} = buildModel(things)
+
+ReactDOM.render(
+	<div>
+		<hr className="two-thirds-width"/>
+		<div>community portal</div>
+		<div className="margin"><img src={logo_img} alt="logo"/></div>
+		<Search data={things} tags={tags}/>
+		<hr className="half-width"/>
+	</div>,
+	document.getElementById('root')
+)
---
+++ reactapp/src/model.js
@@ -0,0 +1,26 @@
+class Thing {
+
+	constructor(
+		id,
+		name,
+		description,
+		text,
+		telephones,
+		emails,
+		url,
+		geoLocation,
+		streetAddress,
+		tags,
+		) {
+			self.id = id,
+			self.name = name,
+			self.description = description,
+			self.text = text,
+			self.telephones = telephones,
+			self.emails = emails,
+			self.url = url,
+			self.geoLocation = geoLocation,
+			self.streetAddress = streetAddress,
+			self.tags = tags,
+	}
+}
